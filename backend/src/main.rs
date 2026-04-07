37fb0440754c1a2f391a7ba0189b97bbe7a3da78
sybausybausybau <sbourr62@gmail.com>
2026-04-07 20:48:55 +0200

    Added an endpoint to delete a password entry

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,7 @@
 use axum::{
     Router, routing::{post}
 };
-use backend::{endpoints::{add_entry_to_user, create_user, get_entries, modify_entry_of_user}, error::ServerError, server_db::ServerDb, structs::AppState};
+use backend::{endpoints::{add_entry_to_user, create_user, get_entries, modify_entry_of_user, delete_entry_of_user}, error::ServerError, server_db::ServerDb, structs::AppState};
 use log::{info};
 use libsodium_rs::ensure_init;
 
@@ -20,6 +20,7 @@ async fn main() -> Result<(), ServerError> {
         .route("/add_entry/{id}", post(add_entry_to_user))
         .route("/get_entry_list/{id}", post(get_entries))
         .route("/modify_entry/{id}", post(modify_entry_of_user))
+        .route("/delete_user/{id}", post(delete_entry_of_user))
         .with_state(state);
 
     info!("Started server on port 3000");
